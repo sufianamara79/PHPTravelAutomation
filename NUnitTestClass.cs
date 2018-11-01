c1ef04f0c7de0939f496e9574f5d1eff5e3e9695
sufianamara79 <sufianamara92@gmail.com>
2018-11-01 17:14:26 +0100

    Initial commit

---
+++ NUnitTestClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+namespace PHPTravelsAutomation
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void TestCase()
+        {
+            ChromeDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.phptravels.net/");
+            driver.Quit();
+
+        }
+    }
+}
