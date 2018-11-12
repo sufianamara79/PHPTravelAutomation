12fbf2a10ea84d2e56e253bb74a68c411ca6116c
sufianamara79 <sufianamara92@gmail.com>
2018-11-12 18:12:54 +0000

    test

---
+++ Tests/HotelSearchTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using PHPTravelsAutomation.PageObjects;
+
+namespace PHPTravelsAutomation.Tests
+{
+    [TestFixture()]
+    public class HotelSearchTests
+    {
+        ChromeDriver driver = new ChromeDriver();
+
+        [Test()]
+        public void TestHotelSearchValid()
+        {
+
+            driver.Navigate().GoToUrl("https://www.phptravels.net/");
+
+
+            HomePage page = new HomePage(driver);
+            page.SearchForHotel("paris", "21", "12", "3");
+
+            Assert.AreEqual("HOTELS", page.GetActiveService());
+
+           
+
+        }
+
+        [TearDown()]
+        public void TestClean()
+        {
+            driver.Quit();
+        }
+    }
+}
